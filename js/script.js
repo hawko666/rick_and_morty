c03085cbb26598b7b1d0563bf0624abe6e140482
manthan_sawant <manthan.sawant@teknopoint.in>
2022-12-02 18:54:52 +0530

    Release

---
+++ js/script.js
@@ -0,0 +1,32 @@
+function customApi() {
+    var xrHp = new XMLHttpRequest();
+    xrHp.open('GET', 'https://rickandmortyapi.com/api/character')
+    xrHp.send();
+    console.log(xrHp);
+
+    xrHp.onload = function () {
+
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(JSON.parse(this.response))
+            var data = JSON.parse(this.response)
+            showData(data.results);
+        }
+
+    }
+}
+var wrap = document.querySelector('.wrapper')
+function showData(a) {
+    for (let i = 0; i < a.length; i++) {
+        var card = '<div class="card">' +
+            '<img src="' + a[i].image + '" alt="img1">' +
+            '<p class="num">' + a[i].id + '</p>' +
+            '<p class="name">' + a[i].name + '</p>' +
+            '</div>'
+
+        wrap.innerHTML += card;
+    }
+}
+
+// var htmlPage = document.querySelector()
+
+customApi();
